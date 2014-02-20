3616b902bc73340ce14037c3e24fdbd4a7a7f09f
lizhemin <lzm1988992hotmail.com>
2014-02-20 14:20:03 -0800

    edited dictionary, added baseline and load dict in Translator.py

--- Translator.py
+++ Translator.py
@@ -1,16 +1,47 @@
+#encoding: utf-8
 import math
+import os
+import codecs
 
 class Translator:
-    def __init__(self):
-        self.filename='dictionary.txt'
+	def __init__(self):
+		self.filename='dictionary.txt'
+		self.dict = {}
+		self.specialWords = [u'了', u'的']
+	
+	def tranlate(self):
+		pass
 
-    def tranlate(self):
-        pass
+	def loadDictionary(self):
+		f = codecs.open(self.filename,'r','utf-8')
+		ls = [ line.strip() for line in f]
+		for i in ls :
+			t = i.split(':')
+			cn_word = t[0]
+			en_words = [w.strip() for w in t[1].split(';')]
+			self.dict[cn_word] = en_words
+		f.close()
+
+	def baseline(self, sentence):
+		words = sentence.split(' ')
+		en_sentence = ''
+		for word in words:
+			if word in self.specialWords:
+				continue
+			if word in self.dict:
+				en_sentence += ' '+self.dict[word][0]
+			else:
+				en_sentence += word
+		en_sentence = en_sentence.strip()
+		return en_sentence
 
 def main():
-    runner=Translator()
-    runner.translate()
-
+	runner=Translator()
+	runner.loadDictionary()
+	f = codecs.open('dev.data.txt','r','utf-8')
+	ls = [line.strip() for line in f]
+	for i in ls :
+		print runner.baseline(i)
 
 if __name__=='__main__':
-    main()
+	main()
